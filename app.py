5d99ffb48bac5b6a350b833645d7d30afc0c459e
koteyevlev <entrant@epd540bt7fbf5pchhbu9.auto.internal>
2019-08-20 18:56:02 +0000

    merge

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 
 
-
 app = Flask(__name__)
 app.config.from_object(Configuration)
-
-app.register_blueprint(posts, url_prefix='/blog')
 
 db = SQLAlchemy(app)
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class   Configuration(object):
     DEBUG = True
-    SQL_ALCHEMY_TRACK_MODIFICATION = False
+    SQL_ALCHEMY_TRACK_MODIFICATIONS = False
     SQL_ALCHEMY_URI = 'mysql+mysqlconnector://entranet:@localhost/flask'
