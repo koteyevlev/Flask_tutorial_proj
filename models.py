2ce772f8c11118f4ecba5080cd7515d2c0423735
Sturgis Krystina <skrystin@mi-q2.21-school.ru>
2019-08-20 21:56:20 +0300

    force

--- models.py
+++ models.py
@@ -2,9 +2,11 @@ from app import db
 from datetime import datetime
 import re
 
+
 def slugify(s):
     pattern = r'[^\w+]'
     return re.sub(pattern, '-', s)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,6 +15,7 @@ class Post(db.Model):
     body = db.Column(db.Text)
     created = db.Column(db.DateTime, default=datetime.now)
 
+
     def __init__(self, *args, **kwards):
         super(Post, self).__init__(*args, **kwards)
         self.generate_slug()
@@ -20,6 +23,6 @@ class Post(db.Model):
     def generate_slug(self):
         if self.title:
             self.slug = slugify(self.title)
-    
+
     def __repr__(self):
         return '<Post id: {}, title: {}>'.format(self.id, self.title)
