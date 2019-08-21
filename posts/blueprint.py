8b8093d1010168df1b2bae9269684579462e6f3b
Sturgis Krystina <skrystin@mi-q2.21-school.ru>
2019-08-21 15:57:37 +0300

    links

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask import render_template
 
-from models import Post
+from models import Post, Tag
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
@@ -15,4 +15,12 @@ def index():
 @posts.route('/<slug>')
 def post_detail(slug):
     post = Post.query.filter(Post.slug == slug).first()
-    return render_template('posts/post_detail.html', post=post)
+    tags = post.tags
+    return render_template('posts/post_detail.html', post=post, tags=tags)
+
+
+@posts.route('/tag/<slug>')
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug == slug).first()
+    posts = tag.posts.all()
+    return render_template('posts/tag_detail.html', tag=tag, posts=posts)
