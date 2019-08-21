477b77e94fe10418966846624784dae8bcc91e23
koteyevlev <entrant@epd540bt7fbf5pchhbu9.auto.internal>
2019-08-21 14:36:08 +0000

    merge

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -9,7 +9,7 @@ posts = Blueprint('posts', __name__, template_folder='templates')
 @posts.route('/')
 def index():
     posts = Post.query.all()
-    return render_template('posts/index.html', posts=posts)
+    return render_template('posts/index.html', posts=posts), 201
 
 
 @posts.route('/<slug>')
